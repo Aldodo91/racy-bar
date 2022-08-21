40b8ae8fd4409ede9331e684f3b35e5e2ac5f811
Aldodo91 <aldo-2@hotmail.it>
2022-08-21 16:11:22 +0200

    add facebook link

--- src/costants/links.js
+++ src/costants/links.js
@@ -1,5 +1,5 @@
 export const links = {
-  fb: "https://facebook.com",
+  fb: "https://www.facebook.com/racycocktail",
   instagram: "https://www.instagram.com/racy.cocktailbar/",
   maps: "https://www.google.com/maps/place/Racy+Bar+%26+Cocktails/@40.6070565,14.3739275,18.88z/data=!4m5!3m4!1s0x0:0xf6a7ae21f8a901fd!8m2!3d40.6070416!4d14.3738448",
   mail: "mailto:carlo.dorso@hotmail.com",
