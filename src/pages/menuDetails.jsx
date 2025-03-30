32fe986aeacd42a8f988d4d02468c5e57d737e5a
Aldodo91 <aldo-2@hotmail.it>
2025-03-30 19:58:57 +0200

    fix 2

--- src/pages/menuDetails.jsx
+++ src/pages/menuDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import * as menuElement from "../costants/allMenuElement";
 import "../style/menuDetails.css";
 import Hambuerger from "../components/hamburger";
